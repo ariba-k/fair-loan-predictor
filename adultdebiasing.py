82a02c3b1c29da25860aaa9c9e579db9e59a6f11
Jashan2348 <60164338+Jashan2348@users.noreply.github.com>
2021-07-21 02:56:33 -0700

    Latest Version Commit
    THIS IS THE LATEST VERSION

--- adultdebiasing.py
+++ adultdebiasing.py
@@ -10,40 +10,71 @@ sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('..'))
 ##----KEY FUNCTIONS----##
 ## Load dataset
-dataset_orig = pd.read_csv(r'C:\Users\Arash\OneDrive\Documents\GitHub\fair-loan-predictor\adult.csv')
-## Drop NULL values
-dataset_orig = dataset_orig.dropna()
-## Drop categorical features
-dataset_orig = dataset_orig.drop(
-    ['workclass', 'fnlwgt', 'education', 'marital-status', 'occupation', 'relationship', 'native-country'], axis=1)
-## Change symbolics to numerics
-dataset_orig['sex'] = np.where(dataset_orig['sex'] == ' Male', 1, 0)
-dataset_orig['race'] = np.where(dataset_orig['race'] != ' White', 0, 1)
-dataset_orig['Probability'] = np.where(dataset_orig['Probability'] == ' <=50K', 0, 1)
-# print(dataset_orig.head(40))
-## Discretize age
-dataset_orig['age'] = np.where(dataset_orig['age'] >= 70, 70, dataset_orig['age'])
-dataset_orig['age'] = np.where((dataset_orig['age'] >= 60) & (dataset_orig['age'] < 70), 60, dataset_orig['age'])
-dataset_orig['age'] = np.where((dataset_orig['age'] >= 50) & (dataset_orig['age'] < 60), 50, dataset_orig['age'])
-dataset_orig['age'] = np.where((dataset_orig['age'] >= 40) & (dataset_orig['age'] < 50), 40, dataset_orig['age'])
-dataset_orig['age'] = np.where((dataset_orig['age'] >= 30) & (dataset_orig['age'] < 40), 30, dataset_orig['age'])
-dataset_orig['age'] = np.where((dataset_orig['age'] >= 20) & (dataset_orig['age'] < 30), 20, dataset_orig['age'])
-dataset_orig['age'] = np.where((dataset_orig['age'] >= 10) & (dataset_orig['age'] < 10), 10, dataset_orig['age'])
-dataset_orig['age'] = np.where(dataset_orig['age'] < 10, 0, dataset_orig['age'])
+def resetDataset():
+    dataset_orig = pd.read_csv(r'C:\Users\jasha\Documents\GitHub\fair-loan-predictor\adult.csv')
+    ## Drop NULL values
+    dataset_orig = dataset_orig.dropna()
+    ## Drop categorical features
+    dataset_orig = dataset_orig.drop(
+        ['workclass', 'fnlwgt', 'education', 'marital-status', 'occupation', 'relationship', 'native-country'], axis=1)
+    ## Change symbolics to numerics
+    dataset_orig['sex'] = np.where(dataset_orig['sex'] == ' Male', 1, 0)
+    dataset_orig['race'] = np.where(dataset_orig['race'] != ' White', 0, 1)
+    dataset_orig['Probability'] = np.where(dataset_orig['Probability'] == ' <=50K', 0, 1)
+    # print(dataset_orig.head(40))
+    ## Discretize age
+    dataset_orig['age'] = np.where(dataset_orig['age'] >= 70, 70, dataset_orig['age'])
+    dataset_orig['age'] = np.where((dataset_orig['age'] >= 60) & (dataset_orig['age'] < 70), 60, dataset_orig['age'])
+    dataset_orig['age'] = np.where((dataset_orig['age'] >= 50) & (dataset_orig['age'] < 60), 50, dataset_orig['age'])
+    dataset_orig['age'] = np.where((dataset_orig['age'] >= 40) & (dataset_orig['age'] < 50), 40, dataset_orig['age'])
+    dataset_orig['age'] = np.where((dataset_orig['age'] >= 30) & (dataset_orig['age'] < 40), 30, dataset_orig['age'])
+    dataset_orig['age'] = np.where((dataset_orig['age'] >= 20) & (dataset_orig['age'] < 30), 20, dataset_orig['age'])
+    dataset_orig['age'] = np.where((dataset_orig['age'] >= 10) & (dataset_orig['age'] < 10), 10, dataset_orig['age'])
+    dataset_orig['age'] = np.where(dataset_orig['age'] < 10, 0, dataset_orig['age'])
 
-print((dataset_orig['sex'] == 0).head(38))
-scaler = MinMaxScaler()
-dataset_orig = pd.DataFrame(scaler.fit_transform(dataset_orig), columns=dataset_orig.columns)
-print(dataset_orig[['age', 'education-num', 'race', 'sex', 'capital-loss', 'hours-per-week']].head(38))
+    # print((dataset_orig['sex'] == 0).head(38))
+    scaler = MinMaxScaler()
+    dataset_orig = pd.DataFrame(scaler.fit_transform(dataset_orig), columns=dataset_orig.columns)
+    # print(dataset_orig[['age', 'education-num', 'race', 'sex', 'capital-loss', 'hours-per-week']].head(38))
+    return dataset_orig
 
+
+
+def splittingDataset(columns, array_remove2):
+    dataset_orig = resetDataset()
+    for newIndex in range(len(array_remove2)):
+        currentIndexName2 = dataset_orig[dataset_orig[columns] == array_remove2[newIndex]].index
+        dataset_orig.drop(currentIndexName2, inplace=True)
+    finalDataset = dataset_orig
+    return finalDataset
+
+def splittingDatasetSecondLayer(columns, array_remove2, initDataset1):
+    initDataset = initDataset1
+    for newIndex in range(len(array_remove2)):
+        currentIndexName2 = initDataset[initDataset[columns] == array_remove2[newIndex]].index
+        initDataset.drop(currentIndexName2, inplace=True)
+    finalDataset = initDataset
+    return finalDataset
+
+
+
+allFemaleDataset = splittingDataset('sex', [1])
+print('Printing all female \n', allFemaleDataset[['age', 'education-num', 'race', 'sex', 'capital-loss', 'hours-per-week']].head(50))
+
+allMaleDataset = splittingDataset('sex', [0])
+print('Printing all male \n', allMaleDataset[['age', 'education-num', 'race', 'sex', 'capital-loss', 'hours-per-week']].head(50))
+
+allBFdataset = splittingDatasetSecondLayer('')
 # divide the data based on sex
 
-dataset_other = dataset_orig.groupby(dataset_orig['sex'] == 0)
-print(dataset_other[['age', 'education-num', 'race', 'sex', 'capital-loss', 'hours-per-week']].head(20))
+dataset_orig = resetDataset()
+
 dataset_orig_male, dataset_orig_female = [x for _, x in dataset_orig.groupby(dataset_orig['sex'] == 0)]
 print(dataset_orig_male[['age', 'education-num', 'race', 'sex', 'capital-loss', 'hours-per-week']].head(20))
 print(dataset_orig_female[['age', 'education-num', 'race', 'sex', 'capital-loss', 'hours-per-week']].head(20))
-# print(dataset_orig_joint[['age', 'education-num', 'race', 'sex', 'capital-loss', 'hours-per-week']].head(20))
+
+
+
 # # divide the data based on race
 # dataset_orig_male_white, dataset_orig_male_black = [x for _, x in dataset_orig_male.groupby(dataset_orig['race'] == 0)]
 # dataset_orig_female_white, dataset_orig_female_black = [x for _, x in dataset_orig_female.groupby(dataset_orig['race'] == 0)]
--- otherdebiasing.py
+++ otherdebiasing.py
@@ -15,66 +15,69 @@ sys.path.append(os.path.abspath('..'))
 #:Training dataset D, Sensitive attribute S, Binary
 # classification model M trained on D, Input space
 # similarity threshold delta
-dataset_orig = pd.read_csv(r'C:\Users\jasha\Documents\GitHub\fair-loan-predictor\TestHMDA.csv', dtype=object)
-###--------------------Sex------------------------
-indexNames1 = dataset_orig[dataset_orig['derived_sex'] == "Sex Not Available"].index
-dataset_orig.drop(indexNames1, inplace=True)
-###-------------------Races-----------------------
-indexNames2 = dataset_orig[dataset_orig['derived_race'] == "American Indian or Alaska Native"].index
-dataset_orig.drop(indexNames2, inplace=True)
-indexNames3 = dataset_orig[dataset_orig['derived_race'] == "Native Hawaiian or Other Pacific Islander"].index
-dataset_orig.drop(indexNames3, inplace=True)
-indexNames4 = dataset_orig[dataset_orig['derived_race'] == "2 or more minority races"].index
-dataset_orig.drop(indexNames4, inplace=True)
-indexNames5 = dataset_orig[dataset_orig['derived_race'] == "Asian"].index
-dataset_orig.drop(indexNames5, inplace=True)
-indexNames6 = dataset_orig[dataset_orig['derived_race'] == "Free Form Text Only"].index
-dataset_orig.drop(indexNames6, inplace=True)
-indexNames7 = dataset_orig[dataset_orig['derived_race'] == "Race Not Available"].index
-dataset_orig.drop(indexNames7, inplace=True)
-####----------------Ethnicity-------------------
-indexNames8 = dataset_orig[dataset_orig['derived_ethnicity'] == "Ethnicity Not Available"].index
-dataset_orig.drop(indexNames8, inplace=True)
-indexNames9 = dataset_orig[dataset_orig['derived_ethnicity'] == "Free Form Text Only"].index
-dataset_orig.drop(indexNames9, inplace=True)
-###----------------Action_taken-----------------
-array_remove = ['4', '5', '6', '7', '8']
-def remove(array_remove):
-    for beginIndex in range(len(array_remove)):
-            currentIndexName = dataset_orig[dataset_orig['action_taken'] == array_remove[beginIndex]].index
-            dataset_orig.drop(currentIndexName, inplace=True)
-remove(array_remove)
-dataset_orig.loc[(dataset_orig.action_taken == '1'),'action_taken'] = 0
-dataset_orig.loc[(dataset_orig.action_taken == '2'),'action_taken'] = 1
-dataset_orig.loc[(dataset_orig.action_taken == '3'),'action_taken'] = 1
-####---------------Reset Indexes----------------
-dataset_orig.reset_index(drop=True, inplace=True)
-# dataset_orig.loc[(dataset_orig.derived_race == 'Joint'),'derived_race']='Joint2'
-# dataset_orig.loc[(dataset_orig.derived_ethnicity == 'Joint'),'derived_ethnicity']='Joint1'
-###----------------Begin Code------------------
-print(dataset_orig[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(70))
-####################################################################################################################################
-dataset_orig = dataset_orig.drop(['activity_year', 'lei', 'derived_msa-md', 'state_code', 'county_code', 'conforming_loan_limit', 'derived_loan_product_type', 'derived_dwelling_category', 'loan_to_value_ratio', 'interest_rate', 'rate_spread', 'total_loan_costs',    'total_points_and_fees', 'origination_charges',    'discount_points', 'lender_credits',  'loan_term',   'prepayment_penalty_term', 'intro_rate_period', 'property_value', 'multifamily_affordable_units', 'income', 'debt_to_income_ratio', 'applicant_ethnicity-2', 'applicant_ethnicity-3', 'applicant_ethnicity-4',    'applicant_ethnicity-5', 'co-applicant_ethnicity-2', 'co-applicant_ethnicity-3', 'co-applicant_ethnicity-4', 'co-applicant_ethnicity-5', 'applicant_race-2', 'applicant_race-3', 'applicant_race-4', 'applicant_race-5', 'co-applicant_race-2',    'co-applicant_race-3', 'co-applicant_race-4', 'co-applicant_race-5', 'applicant_age_above_62', 'co-applicant_age_above_62', 'aus-2','aus-3', 'aus-4',    'aus-5', 'denial_reason-2',    'denial_reason-3', 'denial_reason-4', 'total_units', "applicant_age", "co-applicant_age"], axis=1)
-## Change symbolics to numerics
-dataset_orig.loc[(dataset_orig.derived_sex == 'Female'),'derived_sex']= 0
-dataset_orig.loc[(dataset_orig.derived_sex == 'Male'),'derived_sex']= 1
-dataset_orig.loc[(dataset_orig.derived_sex == 'Joint'),'derived_sex']= 2
-dataset_orig.loc[(dataset_orig.derived_race == 'Black or African American'), 'derived_race'] = 0
-dataset_orig.loc[(dataset_orig.derived_race == 'White'), 'derived_race'] = 1
-dataset_orig.loc[(dataset_orig.derived_race == 'Joint'), 'derived_race'] = 2
-dataset_orig.loc[(dataset_orig.derived_ethnicity == 'Hispanic or Latino'),'derived_ethnicity'] = 0
-dataset_orig.loc[(dataset_orig.derived_ethnicity == 'Not Hispanic or Latino'),'derived_ethnicity'] = 1
-dataset_orig.loc[(dataset_orig.derived_ethnicity == 'Joint'),'derived_ethnicity'] = 2
+def resetDataset():
+    dataset_orig = pd.read_csv(r'C:\Users\jasha\Documents\GitHub\fair-loan-predictor\TestHMDA.csv', dtype=object)
+    ###--------------------Sex------------------------
+    indexNames1 = dataset_orig[dataset_orig['derived_sex'] == "Sex Not Available"].index
+    dataset_orig.drop(indexNames1, inplace=True)
+    ###-------------------Races-----------------------
+    indexNames2 = dataset_orig[dataset_orig['derived_race'] == "American Indian or Alaska Native"].index
+    dataset_orig.drop(indexNames2, inplace=True)
+    indexNames3 = dataset_orig[dataset_orig['derived_race'] == "Native Hawaiian or Other Pacific Islander"].index
+    dataset_orig.drop(indexNames3, inplace=True)
+    indexNames4 = dataset_orig[dataset_orig['derived_race'] == "2 or more minority races"].index
+    dataset_orig.drop(indexNames4, inplace=True)
+    indexNames5 = dataset_orig[dataset_orig['derived_race'] == "Asian"].index
+    dataset_orig.drop(indexNames5, inplace=True)
+    indexNames6 = dataset_orig[dataset_orig['derived_race'] == "Free Form Text Only"].index
+    dataset_orig.drop(indexNames6, inplace=True)
+    indexNames7 = dataset_orig[dataset_orig['derived_race'] == "Race Not Available"].index
+    dataset_orig.drop(indexNames7, inplace=True)
+    ####----------------Ethnicity-------------------
+    indexNames8 = dataset_orig[dataset_orig['derived_ethnicity'] == "Ethnicity Not Available"].index
+    dataset_orig.drop(indexNames8, inplace=True)
+    indexNames9 = dataset_orig[dataset_orig['derived_ethnicity'] == "Free Form Text Only"].index
+    dataset_orig.drop(indexNames9, inplace=True)
+    ###----------------Action_taken-----------------
+    array_remove = ['4', '5', '6', '7', '8']
+    def remove(array_remove):
+        for beginIndex in range(len(array_remove)):
+                currentIndexName = dataset_orig[dataset_orig['action_taken'] == array_remove[beginIndex]].index
+                dataset_orig.drop(currentIndexName, inplace=True)
+    remove(array_remove)
+    dataset_orig.loc[(dataset_orig.action_taken == '1'),'action_taken'] = 0
+    dataset_orig.loc[(dataset_orig.action_taken == '2'),'action_taken'] = 1
+    dataset_orig.loc[(dataset_orig.action_taken == '3'),'action_taken'] = 1
+    ####---------------Reset Indexes----------------
+    dataset_orig.reset_index(drop=True, inplace=True)
+    # dataset_orig.loc[(dataset_orig.derived_race == 'Joint'),'derived_race']='Joint2'
+    # dataset_orig.loc[(dataset_orig.derived_ethnicity == 'Joint'),'derived_ethnicity']='Joint1'
+    ###----------------Begin Code------------------
+    # print(dataset_orig[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(70))
+    ####################################################################################################################################
+    dataset_orig = dataset_orig.drop(['activity_year', 'lei', 'derived_msa-md', 'state_code', 'county_code', 'conforming_loan_limit', 'derived_loan_product_type', 'derived_dwelling_category', 'loan_to_value_ratio', 'interest_rate', 'rate_spread', 'total_loan_costs',    'total_points_and_fees', 'origination_charges',    'discount_points', 'lender_credits',  'loan_term',   'prepayment_penalty_term', 'intro_rate_period', 'property_value', 'multifamily_affordable_units', 'income', 'debt_to_income_ratio', 'applicant_ethnicity-2', 'applicant_ethnicity-3', 'applicant_ethnicity-4',    'applicant_ethnicity-5', 'co-applicant_ethnicity-2', 'co-applicant_ethnicity-3', 'co-applicant_ethnicity-4', 'co-applicant_ethnicity-5', 'applicant_race-2', 'applicant_race-3', 'applicant_race-4', 'applicant_race-5', 'co-applicant_race-2',    'co-applicant_race-3', 'co-applicant_race-4', 'co-applicant_race-5', 'applicant_age_above_62', 'co-applicant_age_above_62', 'aus-2','aus-3', 'aus-4',    'aus-5', 'denial_reason-2',    'denial_reason-3', 'denial_reason-4', 'total_units', "applicant_age", "co-applicant_age"], axis=1)
+    ## Change symbolics to numerics
+    dataset_orig.loc[(dataset_orig.derived_sex == 'Female'),'derived_sex']= 0
+    dataset_orig.loc[(dataset_orig.derived_sex == 'Male'),'derived_sex']= 1
+    dataset_orig.loc[(dataset_orig.derived_sex == 'Joint'),'derived_sex']= 2
+    dataset_orig.loc[(dataset_orig.derived_race == 'Black or African American'), 'derived_race'] = 0
+    dataset_orig.loc[(dataset_orig.derived_race == 'White'), 'derived_race'] = 1
+    dataset_orig.loc[(dataset_orig.derived_race == 'Joint'), 'derived_race'] = 2
+    dataset_orig.loc[(dataset_orig.derived_ethnicity == 'Hispanic or Latino'),'derived_ethnicity'] = 0
+    dataset_orig.loc[(dataset_orig.derived_ethnicity == 'Not Hispanic or Latino'),'derived_ethnicity'] = 1
+    dataset_orig.loc[(dataset_orig.derived_ethnicity == 'Joint'),'derived_ethnicity'] = 2
 
-scaler = MinMaxScaler()
-dataset_orig = pd.DataFrame(scaler.fit_transform(dataset_orig), columns=dataset_orig.columns)
-# print(dataset_orig[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(20))
-# divide the data based on sex
-# dataset_new = dataset_orig.groupby(dataset_orig['derived_sex'] == 0)
-# print(dataset_new[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(20))
+    scaler = MinMaxScaler()
+    dataset_orig = pd.DataFrame(scaler.fit_transform(dataset_orig), columns=dataset_orig.columns)
+    # print(dataset_orig[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(20))
+    # divide the data based on sex
+    # dataset_new = dataset_orig.groupby(dataset_orig['derived_sex'] == 0)
+    # print(dataset_new[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(20))
+    return dataset_orig
 
 
 def splittingDataset(columns, array_remove2):
+    dataset_orig = resetDataset()
     for newIndex in range(len(array_remove2)):
         currentIndexName2 = dataset_orig[dataset_orig[columns] == array_remove2[newIndex]].index
         dataset_orig.drop(currentIndexName2, inplace=True)
@@ -83,17 +86,45 @@ def splittingDataset(columns, array_remove2):
 
 
 
+def splittingDatasetSecondLayer(columns, array_remove2, initDataset1):
+    initDataset = initDataset1
+    for newIndex in range(len(array_remove2)):
+        currentIndexName2 = initDataset[initDataset[columns] == array_remove2[newIndex]].index
+        initDataset.drop(currentIndexName2, inplace=True)
+    finalDataset = initDataset
+    return finalDataset
+
+
 
 allFemaleDataset = splittingDataset('derived_sex', [.5,1])
 print('Printing all female \n', allFemaleDataset[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(50))
+
 allMaleDataset = splittingDataset('derived_sex', [0,1])
-print('Printing all male \n', allMaleDataset)
+print('Printing all male \n', allMaleDataset[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(50))
+
 allJointDataset = splittingDataset('derived_sex', [0,0.5])
+print('Printing all Joint \n', allJointDataset[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(50))
+
+
+allBFdataset = splittingDatasetSecondLayer('derived_race', [.5, 1], allFemaleDataset)
+print('Printing all BFs \n', allBFdataset[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(50))
+allBFdataset.to_csv(r'C:\Users\jasha\Documents\GitHub\fair-loan-predictor\allBFDataset.csv')
+
+allBFNHOLdataset = splittingDatasetSecondLayer('derived_ethnicity', [0, 1], allBFdataset)
+print('Printing all BFNHOLs \n', allBFNHOLdataset[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(50))
+
+allBFHOLdataset = splittingDatasetSecondLayer('derived_ethnicity', [.5, 1], allBFdataset)
+print('Printing all Jashan \n', allBFHOLdataset[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
+
+
+# divide the data based on sex
 # newIndexName = dataset_orig[dataset_orig['derived_sex'] == .5].index
 # dataset_orig.drop(newIndexName, inplace=True)
 # newIndexName2 = dataset_orig[dataset_orig['derived_sex'] == 1].index
 # dataset_orig.drop(newIndexName2, inplace=True)
 # print(dataset_orig[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(20))
+
+# dataset_orig = resetDataset()
 
 # dataset_orig_male, dataset_orig_female = [x for _, x in dataset_orig.groupby(dataset_orig['derived_sex'] == 0)]
 # print(dataset_orig_male[['derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']].head(20))
