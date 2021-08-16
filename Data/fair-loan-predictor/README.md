208bb11a7b756c1df5a494be4eb7100dcbb3a9d0
Arash2348 <46118628+Arash2348@users.noreply.github.com>
2021-08-15 22:58:39 -0700

    Oversampling and Situation Finished (Not Checked, however)

--- Data/fair-loan-predictor/README.md
+++
@@ -1,2 +0,0 @@
-# fair-loan-predictor
-Predicts whether a mortgage loan application should be accepted. Uses a fair interpretable model based on a counter factual fairness metric. 
--- newactualdebiasing.py
+++ newactualdebiasing.py
@@ -6,9 +6,9 @@ from itertools import product
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.utils import resample
-from aribabalancing import balance
+
 from SMOTE import smote
+from Generate_Samples import generate_samples
 
 sys.path.append(os.path.abspath('..'))
 
@@ -21,7 +21,7 @@ output_file = base_path + '\\Data\\debiased_state_CT.csv'
 
 
 dataset_orig = pd.read_csv(input_file, dtype=object)
-print('Data', dataset_orig.shape())
+print('Data', dataset_orig.shape)
 print(dataset_orig[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
 
 ###------------Scaling Function to be used later----------------------
@@ -48,6 +48,8 @@ def preprocessing(dataset_orig):
     dataset_orig['derived_sex'] = dataset_orig['derived_sex'].replace(['Female', 'Male', 'Joint'],
                                                                       [0, 1, 2])
     print('sex: ' + str(dataset_orig.shape))
+    print(dataset_orig[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
+
     ###-------------------Races-----------------------
     dataset_orig = dataset_orig[(dataset_orig['derived_race'] == 'White') |
                                 (dataset_orig['derived_race'] == 'Black or African American') |
@@ -55,6 +57,8 @@ def preprocessing(dataset_orig):
     dataset_orig['derived_race'] = dataset_orig['derived_race'].replace(['Black or African American', 'White', 'Joint'],
                                                                         [0, 1, 2])
     print('race: ' + str(dataset_orig.shape))
+    print(dataset_orig[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
+
     ####----------------Ethnicity-------------------
     dataset_orig = dataset_orig[(dataset_orig['derived_ethnicity'] == 'Hispanic or Latino') |
                                 (dataset_orig['derived_ethnicity'] == 'Not Hispanic or Latino') |
@@ -62,6 +66,8 @@ def preprocessing(dataset_orig):
     dataset_orig['derived_ethnicity'] = dataset_orig['derived_ethnicity'].replace(['Hispanic or Latino', 'Not Hispanic or Latino', 'Joint'],
                                                                                   [0, 1, 2])
     print('ethnicity: ' + str(dataset_orig.shape))
+    print(dataset_orig[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
+
     ###----------------Action_Taken-----------------
     dataset_orig = dataset_orig[(dataset_orig['action_taken'] == '1') |
                                 (dataset_orig['action_taken'] == '2') |
@@ -70,22 +76,28 @@ def preprocessing(dataset_orig):
     dataset_orig['action_taken'] = dataset_orig['action_taken'].replace(['1', '2', '3'],
                                                                         [1, 0, 0])
 
+    print(dataset_orig[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
+
     ######----------------Loan Product-------------------
     # assigns each unique categorical value a unique integer id
     dataset_orig['derived_loan_product_type'] = dataset_orig['derived_loan_product_type'].astype('category').cat.codes
 
     print('loan product: ' + str(dataset_orig.shape))
+    print(dataset_orig[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
 
     ####---------------Scale Dataset---------------
     dataset_orig = scale_dataset(dataset_orig)
 
+    print(dataset_orig[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
+
     ##----------------Solve NAN problem-----------
+
     dataset_orig = dataset_orig.apply(pd.to_numeric)
     dataset_orig = dataset_orig.dropna()
     float_col = dataset_orig.select_dtypes(include=['float64'])
 
-    for col in float_col.columns.values:
-        dataset_orig[col] = dataset_orig[col].astype('int64')
+    # for col in float_col.columns.values:
+    #     dataset_orig[col] = dataset_orig[col].astype('int64')
     ####---------------Reset Indexes----------------
     dataset_orig.reset_index(drop=True, inplace=True)
 
@@ -116,17 +128,20 @@ class EmptyList(Exception):
 ind_cols = ['derived_ethnicity', 'derived_race', 'derived_sex']
 dep_col = 'action_taken'
 
-
-def split_dataset(processed_scaled_df, ind_cols):
+def get_unique_df(processed_scaled_df,ind_cols):
     uniques = [processed_scaled_df[i].unique().tolist() for i in ind_cols]
     unique_df = pd.DataFrame(product(*uniques), columns=ind_cols)
-    print(unique_df)
+    return unique_df
+
+
+def split_dataset(processed_scaled_df, ind_cols):
+    unique_df = get_unique_df(processed_scaled_df, ind_cols)
     combination_df = [pd.merge(processed_scaled_df, unique_df.iloc[[i]], on=ind_cols, how='inner') for i in
                       range(unique_df.shape[0])]
 
     return combination_df
 
-
+global_unique_df = get_unique_df(processed_scaled_df, ind_cols)
 combination_df = split_dataset(processed_scaled_df, ind_cols)
 
 combination_names = ['nhol_w_m', 'nhol_w_f', 'nhol_w_j',
@@ -140,8 +155,8 @@ combination_names = ['nhol_w_m', 'nhol_w_f', 'nhol_w_j',
                      'j_b_m', 'j_b_f','j_b_j']
 
 # # uncomment to see which name is associated with which combination
-for n, c in zip(combination_names, combination_df):
-    print(n, c[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
+# for n, c in zip(combination_names, combination_df):
+#     print(n, c[['derived_msa-md', 'derived_ethnicity', 'derived_race', 'derived_sex', 'action_taken']])
 
 # nhol_w_m = combination_df[0]
 # nhol_w_f = combination_df[1]
@@ -176,7 +191,6 @@ def get_max_val(max_val):
     for df in combination_df:
         temp1 = 0
         temp0 = 0
-        print('The Distribution\n', df['action_taken'].value_counts())
         try:
             temp1 = len(df[(df['action_taken'] == 1)])
             temp0 = len(df[(df['action_taken'] == 0)])
@@ -190,9 +204,8 @@ def get_max_val(max_val):
 
     return max_val
 
-print("Here:\n", get_max_val(max_val))
-
-
+maximum = get_max_val(max_val)
+print("Here is the aim:", maximum)
 ####
 def smote_balance(c):
     def apply_smote(df):
@@ -203,7 +216,6 @@ def smote_balance(c):
         df.columns = cols
         return df
 
-
     X_train, y_train = c.loc[:, c.columns != 'action_taken'], c['action_taken']
 
     train_df = X_train
@@ -211,22 +223,81 @@ def smote_balance(c):
 
     train_df = apply_smote(train_df)
 
-    print('Next Distribution\n', train_df['action_taken'].value_counts())
+
+    return train_df
 
 
-classifiers = [smote_balance(c) for c in combination_df]
+balanced_df_list = [smote_balance(c) for c in combination_df]
+
+
+super_balanced = []
+for df in balanced_df_list:
+    temp_val_0 = len(df[(df['action_taken'] == 0)])
+    temp_val_1 = len(df[(df['action_taken'] == 1)])
+
+    num_increase_of_0 = maximum - temp_val_0
+    num_increase_of_1 = maximum - temp_val_1
+
+    print('Before Distribution\n', df['action_taken'].value_counts())
+
+    df = generate_samples(num_increase_of_0, df, 'HMDA', 0) ##@params = {value_of_increase, dataframe, dataset_name, only_this_action_taken_value}
+    df = generate_samples(num_increase_of_1, df, 'HMDA', 1)
+
+    print('After Distribution\n', df['action_taken'].value_counts())
+    super_balanced.append(df)
 
 
 
+def concat_and_shuffle(df):
+    concat_df = pd.concat(df)
+    concat_df = concat_df.sample(frac=1).reset_index(drop=True)
+
+    print('shuffle:', concat_df)
+    return concat_df
+
+new_dataset_orig = concat_and_shuffle(super_balanced)
+
+#-----------------Situation Testing-------------------
+X_train, y_train = new_dataset_orig.loc[:, new_dataset_orig.columns != 'action_taken'], new_dataset_orig['action_taken']
+
+clf = LogisticRegression(C=1.0, penalty='l2', solver='liblinear', max_iter=100)
+clf.fit(X_train,y_train)
+
+removal_list = []
+pred_list = []
+
+##Gets you bad rows that need to be deleted
+for index,row in new_dataset_orig.iterrows():
+    row_ = [row.values[0:len(row.values) - 1]]
+    for other_index, other_row in global_unique_df.iterrows():
+        current_comb = [other_row.values[0:len(other_row.values) - 1]]  ## indexes are 2, 3, 4 for ethnicity, race, sex respectively
+        print('current_com[0]', current_comb[0])
+        row_[0][2] = current_comb[0] ##don't know confirm what this is
+        row_[0][3] = current_comb[1]
+        row_[0][4] = current_comb[2]
+        y_current_pred = clf.predict(row_)
+        pred_list.append(y_current_pred)
+    num_unique_vals = len(set(pred_list))
+
+    print('Num Uniue Values:', num_unique_vals)
+
+    if num_unique_vals > 1:
+        removal_list.append(index)
+    elif num_unique_vals == 0:
+        raise EmptyList
+
+removal_list = set(removal_list)
+print(len(removal_list))
 
 
+print(new_dataset_orig.shape)
+df_removed = pd.DataFrame(columns=new_dataset_orig.columns)
 
-
-
-
-
-
-
+for index,row in new_dataset_orig.iterrows():
+    if index in removal_list:
+        df_removed = df_removed.append(row, ignore_index=True)
+        balanced_and_situation_df = new_dataset_orig.drop(index)
+        print(balanced_and_situation_df.shape)
 
 
 
